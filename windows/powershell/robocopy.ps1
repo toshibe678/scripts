3f5e159a5a032f8dcce5538af33fedf42caba68c
toshi <toshi@toshi.click>
2024-04-12 21:09:42 +0900

    robocopyでバックアップするスクリプト作成

---
+++ windows/powershell/robocopy.ps1
@@ -0,0 +1,8 @@
+# taskschd.msc でタスクスケジューラを開いて起動後に実行するように設定
+
+# ドライブをマウント
+$SecureString = ConvertTo-SecureString "ロック解除パスワード" -AsPlainText -Force
+Unlock-BitLocker -MountPoint "E:" -Password $SecureString
+
+# robocopy実行
+robocopy Z:\ E:\ /LOG:C:\Users\toshi\robocopy-Log.log /MIR /DCOPY:DT /COPY:DT /FFT /TEE /NP /XJF /XJD /MT:128 /COMPRESS /R:0 /W:0 /XD "System Volume Information" "$RECYCLE.BIN"
