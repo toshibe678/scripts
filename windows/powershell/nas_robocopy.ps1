dd372d9bbee7600f30d74e2a1b91fb7fd5acfa89
toshi <toshi@toshi.click>
2024-09-03 19:49:27 +0900

    NASの除外ファイル追加

--- windows/powershell/nas_robocopy.ps1
+++ windows/powershell/nas_robocopy.ps1
@@ -5,4 +5,4 @@
 # Unlock-BitLocker -MountPoint "E:" -Password $SecureString
 
 # robocopy実行
-robocopy Y:\ E:\ /LOG:C:\Users\toshi\robocopy-Log.log /MIR /DCOPY:DT /COPY:DT /FFT /TEE /NP /XJF /XJD /MT:128 /COMPRESS /R:0 /W:0 /XD "System Volume Information" "$RECYCLE.BIN"
+robocopy Y:\ E:\ /LOG:C:\Users\toshi\robocopy-Log.log /MIR /DCOPY:DT /COPY:DT /FFT /TEE /NP /XJF /XJD /MT:128 /COMPRESS /R:0 /W:0 /XD "@Recycle" "@Recently-Snapshot" "System Volume Information" "$RECYCLE.BIN"
